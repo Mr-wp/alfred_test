3beb4abeedf2deb19412c21f26f4cd243edee1f1
wangping <624265900@qq.com>
2023-03-10 17:46:25 +0800

    composer 包

---
+++ src/Common/Test.php
@@ -0,0 +1,23 @@
+<?php
+/**
+ *Note:
+ *2023/3/10-17:22
+ *Author: Alfred
+ *Email: silentwolf_wp@163.com
+ */
+
+namespace Alfred\Test\Common;
+
+
+class Test
+{
+
+    public function index(){
+        echo 'hello world';
+    }
+    public function check(){
+        if(in_array($_SERVER['HTTP_HOST'],['www.findwp.cn'])){
+            echo '非授权域名www.findwp.cn访问';die;
+        }
+    }
+}
